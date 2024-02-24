b6e1bc47753064cb248291fe48366fe3ec96d52b
LibardoVega <libardo0047@gmail.com>
2024-02-23 20:35:14 -0500

    graphene ok

---
+++ schema.py
@@ -0,0 +1,14 @@
+import graphene
+import eventos.schema
+from eventos.mutations import Mutation as EventMutations
+
+
+class Query(eventos.schema.QueryEvents, graphene.ObjectType):
+    pass
+
+
+class Mutation(EventMutations):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
